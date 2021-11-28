638e4c372c0ffa574f507999e6b26c39d73a665d
David Deloso <daviddeloso@Davids-MBP-2.cable.rcn.com>
2021-11-28 16:21:45 -0600

    update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(process.env.PORT || 5000, () => {
+  console.log('listening on *:5000');
 });
 
